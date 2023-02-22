cd3c3dfd99dddbbbedbd4b0b6b35a7a51c14dc32
Thomas Hertweck <Thomas.Hertweck@kit.edu>
2023-02-22 10:49:53 +0100

    Cleanup of globvar_struct comments; no actual code changes.

--- src/globvar_struct.h
+++ src/globvar_struct.h
@@ -15,13 +15,13 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with SOFI2D. See file COPYING and/or 
-  * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
 --------------------------------------------------------------------------*/
 
 /*------------------------------------------------------------------------
  * globvar_struct.h - global variables of viscoelastic 2D FD program
- * generally, for the names of the global variables uppercase letters are used
- *
+ * generally, for the names of the global variables uppercase letters 
+ * are used.
  * ----------------------------------------------------------------------*/
 
 #ifndef GLOBVAR_STRUCT_H_INCLUDED
@@ -41,12 +41,12 @@ typedef struct {
     char MFILE[STRING_SIZE];    // model file name
     int NX;                     // number of grid points in x-direction
     int NY;                     // number of grid points in y-direction (depth)
-    /*Attenuation */
+    // Attenuation
     int L;                      // number of relaxation parameters
-    float F_REF;                 // reference frequency for dispersion [Hz]
+    float F_REF;                // reference frequency for dispersion [Hz]
     float *FL;                  // frequency of each relaxation parameters [Hz]
     float TAU;                  // ratio of retardation and relaxation time
-    /*Source */
+    // Source
     int SOURCE_TYPE;            // type of source
     int SOURCE_SHAPE;           // shape of source-signal
     char SIGNAL_FILE[STRING_SIZE];  // name of external signal file
@@ -56,9 +56,9 @@ typedef struct {
     float PLANE_WAVE_DEPTH;     // depth of plane wave excitation [meter]
     float PLANE_WAVE_ANGLE;     // dip of plane wave from vertical [degree]
     float TS;                   // duration of source signal [in second]
-    float XS;                   // Source location ??
-    float YS;                   // Source location ??
-    /*Receiver */
+    float XS;                   // Source location
+    float YS;                   // Source location
+    // Receiver
     int READREC;                // switch to read receiver positions from file
     char REC_FILE[STRING_SIZE]; // name of external receiver file
     float REFREC[4];            // reference point for receiver coordinate system
@@ -67,7 +67,7 @@ typedef struct {
     float YREC1;                // y-position of first receiver [m]
     float YREC2;                // y-position of last receiver [m]
     float NGEOPH;               // distance between two adjacent receivers [gridpoints]; in auto mode NGEOPH will be 
-    // calculated from model dimensions, type integer is incorrect
+                                // calculated from model dimensions, type integer is incorrect
     int NTRG;                   // global number of receiver
     int NTR;                    // number of receivers per PE
     int REC_ARRAY;              // number of receivers in 1D receiver array
@@ -77,8 +77,7 @@ typedef struct {
 //}
 
 //struct IOVar {
-    //IO
-    /*Seismograms */
+    // Seismograms
     int SEISMO;                 // switch to output components of seismograms
     int NDT;                    // sampling rate of seismograms [timesteps DT]
     int NS;                     // number of samples of seismogram
@@ -90,7 +89,7 @@ typedef struct {
     float **SECTIONCURL;        // buffer for seismogram output (curl-component)
     float **SECTIONDIV;         // buffer for seismogram output (div-component)
     float **SEISMO_FULLDATA;    // buffer for merge of seismograms
-    /*Snapshots */
+    // Snapshots
     int SNAP;                   // switch to output of snapshots
     int SNAP_FORMAT;            // data output format for snapshots
     char SNAP_FILE[STRING_SIZE];    // name of output file of snapshots
@@ -99,7 +98,7 @@ typedef struct {
     float TSNAPINC;             // increment between snapshots [s]
     int IDX;                    // increment in x-direction [gridpoints]
     int IDY;                    // increment in y-direction [gridpoints]
-    /*Others */
+    // Others
     int WRITE_MODELFILES;       // switch to output model files
     int SIGOUT;                 // switch to output source wavelet
     int SIGOUT_FORMAT;          // data output format for source wavelet
@@ -115,60 +114,48 @@ typedef struct {
     WEQTYPE WEQ;                // wave equation
     float DH;                   // spacial increment [m]
     int FDORDER;                // spatial FD order
-    int ND;                     //// gv->FDORDER / 2
-    int FDO3;                   //// gv->ND * 2 ???
+    int ND;                     // gv->FDORDER / 2
+    int FDO3;                   // gv->ND * 2
     float TIME;                 // time (of modelling) [s]
     float DT;                   // time increment (of modelling) [s]
     int FDORDER_TIME;           // temporal FD order
     int NT;                     //// number of timesteps (=iround(TIME/DT))
     FDop_s_fct FDOP_S;          // function pointer for FD operator
     FDop_v_fct FDOP_V;          // function pointer for FD operator
-    /* MPI-variables */
+    // MPI-variables
     int MAXRELERROR;            // switch of maximum relative group velocity error
     int NPROCX;                 // number of processors in x-direction
     int NPROCY;                 // number of processors in y-direction
-    int *GX;                    //// subgrid array in x-direction
-    int *GY;                    //// subgrid array in y-diection
-    char *BUFF_ADDR;            //// buffer for buffering messages
-    int BUFFSIZE;               //// size of buffer for buffering messages
-    int NPROC;                  //// number of processors (=NPROCX*NPROCY)
-    int NP;                     //// number of processors from mpirun command
-    int MPID;                   //// ID of processor
-    int INDEX[5];               //// ID of neighboring processes %% Why 5??? - should be just 4
-    int POS[3];                 //// processor location in the 3D logical processor array (MPID%NPROCX; MPID/NPROCX) %% Why 3?? - should be just 2
-    int IENDX;                  //// size of domain in x-direction (=NX/NPROCX)
-    int IENDY;                  //// size of domain in y-direction (=NY/NPROCY)
-    int NXG;                    //// number of grid points in x-direction (global)
-    int NYG;                    //// number of grid points in y-direction (global)
-    const int TAG1;             //// ??? place holder ???
-    const int TAG2;             //// ??? place holder ???
-    const int TAG3;             //// ??? place holder ???
-    const int TAG4;             //// ??? place holder ???
-    const int TAG5;             //// ??? place holder ???
-    const int TAG6;             //// ??? place holder ???
+    int *GX;                    // subgrid array in x-direction
+    int *GY;                    // subgrid array in y-diection
+    char *BUFF_ADDR;            // buffer for buffering messages
+    int BUFFSIZE;               // size of buffer for buffering messages
+    int NPROC;                  // number of processors (=NPROCX*NPROCY)
+    int NP;                     // number of processors from mpirun command
+    int MPID;                   // ID of processor
+    int INDEX[5];               // ID of neighboring processes (slot 0 unused; slots 1...4 used to be consistent with util.c)
+    int POS[3];                 // processor location in the 3D logical proc array (MPID%NPROCX; MPID/NPROCX) (slot 0 unused)
+    int IENDX;                  // size of domain in x-direction (=NX/NPROCX)
+    int IENDY;                  // size of domain in y-direction (=NY/NPROCY)
+    int NXG;                    // number of grid points in x-direction (global)
+    int NYG;                    // number of grid points in y-direction (global)
+    const int TAG1;             // MPI tag
+    const int TAG2;             // MPI tag
+    const int TAG3;             // MPI tag
+    const int TAG4;             // MPI tag
+    const int TAG5;             // MPI tag
+    const int TAG6;             // MPI tag
     // Boundary
     int FREE_SURF;              // switch to apply free surface at the top of the model
     int BOUNDARY;               // switch to apply periodic boundary condition at edges
     int ABS_TYPE;               // type of the absorbing boundary
     int FW;                     // width of absorbing frame [gridpoints]
     float DAMPING;              // attenuation at the edges of the grid [%]
-    /* PML-Parameters */
+    // PML-Parameters
     float NPOWER;               // exponent for calculation of damping profile
     float K_MAX_CPML;           // 
     float FPML;                 // dominant signal frequency (usually FC) [Hz]
     float VPPML;                // attenuation velocity within the PML boundary [m/s]
 } GlobVar;
-
-/* Parameters not used - please double check! */
-//    int   RSG;                          //// ??? NOT USED ??? - rotated staggered grid; spatial adaptive Code variables
-//    float SRCPOSXYZ[3]={0.0, 0.0, 0.0}; //// !!! NOT USED !!!
-//    int   NSRC;                         //// !!! NOT USED !!!
-//    int   NPSP;                         //// !!! NOT USED !!!
-//    int   DC;                           //// !!! NOT USED !!!
-//    int   check_id;                     //// !!! NOT USED !!!
-//    int   cfgt_id;                      //// !!! NOT USED !!!
-//    int   cfgt;                         //// !!! NOT USED !!!
-//    int   jumpid;                       //// !!! NOT USED !!!
-//    float DH1;                          //// !!! NOT USED !!!
 
 #endif
