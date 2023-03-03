a681950247c4d8c34e7f39da290b4c24565a5e14
Thomas Hertweck <Thomas.Hertweck@kit.edu>
2023-03-03 10:47:03 +0100

    Removed NP from globvar struct (only req. in one place); added log of Cartesian MPI grid; added GGRID variable in globvar struct to store which global grid points each processor handles in preparation for subdomains of unequal size.

--- src/initproc.c
+++ src/initproc.c
@@ -29,9 +29,11 @@
 
 void initproc(GlobVar *gv)
 {
-    if ((gv->NPROC != gv->NP) && (gv->MPID == 0)) {
+    int nmpi;
+    MPI_Comm_size(MPI_COMM_WORLD, &nmpi);
+    if ((gv->NPROC != nmpi) && (gv->MPID == 0)) {
         log_error("You specified NPROCX*NPROCY=%d (in json file) but number of MPI processes set to NP=%d.\n",
-                  gv->NPROC, gv->NP);
+                  gv->NPROC, nmpi);
         log_fatal("Number of MPI processes (NP) and number of processing elements (NPROCX*NPROCY) differ!\n");
     }
 
@@ -65,49 +67,66 @@ void initproc(GlobVar *gv)
     if (gv->POS[2] == gv->NPROCY - 1)
         gv->INDEX[4] = gv->MPID + gv->NPROCX - (gv->NPROCX * gv->NPROCY);
 
-    log_debug("PE %d; col %d; row %d; left-right neighbor %d,%d; upper-lower neighbor %d,%d\n",
-              gv->MPID, gv->POS[1], gv->POS[2], gv->INDEX[1], gv->INDEX[2], gv->INDEX[3], gv->INDEX[4]);
+    /* GGRID are the global model indices [1,NYG][1,NXG] handled by this logical processor, *
+     * 1=left (idx xmin), 2=right (idx xmax), 3=upper (idx ymin), 4=lower (idx ymax)        */
+    gv->GGRID[1] = 1;
+    for (int i = 0; i < gv->POS[1]; ++i) {
+        gv->GGRID[1] += gv->NX;
+    }
+    gv->GGRID[2] = gv->GGRID[1] + (gv->NX - 1);
+    gv->GGRID[3] = 1;
+    for (int i = 0; i < gv->POS[2]; ++i) {
+        gv->GGRID[3] += gv->NY;
+    }
+    gv->GGRID[4] = gv->GGRID[3] + (gv->NY - 1);
+
+    log_debug("PE %d; col %d; row %d; neighbors l-r %d,%d, u-l %d,%d; grid points xb-xe %d,%d yb-ye %d,%d\n",
+              gv->MPID, gv->POS[1], gv->POS[2], gv->INDEX[1], gv->INDEX[2], gv->INDEX[3], gv->INDEX[4],
+              gv->GGRID[1], gv->GGRID[2], gv->GGRID[3], gv->GGRID[4]);
 
     return;
 }
 
-/* Example: NPROX=5, NPROCY=4 => NPROC=20           */
-/*                                                  */
-/* ================================================ */
-/*                                                  */
-/*  0               0: 4,1                  0: 15,5 */
-/*  1               1: 0,2                  0: 16,6 */
-/*  2               2: 1,3                  0: 17,7 */
-/*  3               3: 2,4                  0: 18,8 */
-/*  4               4: 3,0                  0: 19,9 */
-/*  5               0: 9,6                  1: 0,10 */
-/*  6               1: 5,7                  1: 1,11 */
-/*  7               2: 6,8                  1: 2,12 */
-/*  8               3: 7,9                  1: 3,13 */
-/*  9               4: 8,5                  1: 4,14 */
-/*  10              0: 14,11                2: 5,15 */
-/*  11              1: 10,12                2: 6,16 */
-/*  12              2: 11,13                2: 7,17 */
-/*  13              3: 12,14                2: 8,18 */
-/*  14              4: 13,10                2: 9,19 */
-/*  15              0: 19,16                3: 10,0 */
-/*  16              1: 15,17                3: 11,1 */
-/*  17              2: 16,18                3: 12,2 */
-/*  18              3: 17,19                3: 13,3 */
-/*  19              4: 18,15                3: 14,4 */
-/*                                                  */
-/* ================================================ */
-/*                                                  */
-/*       0         1        2       3       4       */
-/*       x         x        x       x       x       */
-/*                                                  */
-/*       5         6        7       8       9       */
-/*       x         x        x       x       x       */
-/*                                                  */
-/*      10        11       12      13      14       */
-/*       x         x        x       x       x       */
-/*                                                  */
-/*      15        16       17      18      19       */
-/*       x         x        x       x       x       */
-/*                                                  */
-/* ================================================ */
+/* Example: NPROCX=5, NPROCY=4 => NPROC=20          *
+ * ================================================ *
+ * Rank=MPID, x=POS[1], y=POS[2]                    *
+ * l=INDEX[1], r=INDEX[2], u=INDEX[3], l=INDEX[4]   *
+ * ================================================ *
+ *  Rank            x: l,r             y: u,l       *
+ *  ----            -------            -------      *
+ *  0               0: 4,1             0: 15,5      *
+ *  1               1: 0,2             0: 16,6      *
+ *  2               2: 1,3             0: 17,7      *
+ *  3               3: 2,4             0: 18,8      *
+ *  4               4: 3,0             0: 19,9      *
+ *  5               0: 9,6             1: 0,10      *
+ *  6               1: 5,7             1: 1,11      *
+ *  7               2: 6,8             1: 2,12      *
+ *  8               3: 7,9             1: 3,13      *
+ *  9               4: 8,5             1: 4,14      *
+ *  10              0: 14,11           2: 5,15      *
+ *  11              1: 10,12           2: 6,16      *
+ *  12              2: 11,13           2: 7,17      *
+ *  13              3: 12,14           2: 8,18      *
+ *  14              4: 13,10           2: 9,19      *
+ *  15              0: 19,16           3: 10,0      *
+ *  16              1: 15,17           3: 11,1      *
+ *  17              2: 16,18           3: 12,2      *
+ *  18              3: 17,19           3: 13,3      *
+ *  19              4: 18,15           3: 14,4      *
+ *                                                  *
+ * ================================================ *
+ *                                                  *
+ *       0         1        2       3       4       *
+ *       x         x        x       x       x       *
+ *                                                  *
+ *       5         6        7       8       9       *
+ *       x         x        x       x       x       *
+ *                                                  *
+ *      10        11       12      13      14       *
+ *       x         x        x       x       x       *
+ *                                                  *
+ *      15        16       17      18      19       *
+ *       x         x        x       x       x       *
+ *                                                  *
+ * ================================================ */
