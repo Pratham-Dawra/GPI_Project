7b2a3bae9bfdfe299ed7208fd43af253aa7c9325
Thomas Hertweck <Thomas.Hertweck@kit.edu>
2023-03-07 19:50:48 +0100

    Bugfix in initsrc if RUN_MULTIPLE_SHOTS=1; led to crash as buffers weren't free'ed correctly and a domain without source was trying to write a signature; also fixed SU header sx/gx of signature output file.

--- src/initsrc.c
+++ src/initsrc.c
@@ -19,48 +19,51 @@
 --------------------------------------------------------------------------*/
 
 /* -------------------------------------------------------------
- * Setting source paramters.
+ * Setting source parameters.
  * -------------------------------------------------------------*/
 
 #include "fd.h"
 #include "logging.h"
 #include "macros.h"
+#include <math.h>
+#include <stdio.h>
 
 void initsrc(int ishot, int nshots, AcqVar *acq, GlobVar *gv)
 {
     int nt;
-    int **dummy = NULL;
-    /*Limits for local grids defined in subgrid_bounds.c */
     char sigf[STRING_SIZE * 2], file_ext[5];
-    /* Memory allocation for saving the current source position */
-    float **srcpos_current = matrix(1, NSPAR, 1, 1);
 
-    for (nt = 1; nt <= NSPAR; nt++) {
-        srcpos_current[nt][1] = acq->srcpos[nt][ishot];
+    if (acq->nsrc_loc > 0) {
+        free_matrix(acq->signals, 1, acq->nsrc_loc, 1, gv->NT);
+        acq->signals = NULL;
+        free_matrix(acq->srcpos_loc, 1, NSPAR, 1, 1);
+        acq->srcpos_loc = NULL;
+        acq->nsrc_loc = 0;
     }
 
     if (gv->RUN_MULTIPLE_SHOTS) {
-        log_info("Starting simulation for shot %d of %d.\n", ishot, nshots);
-
+        log_infoc(0, "Starting simulation for shot %d of %d.\n", ishot, nshots);
+        float **srcpos_current = matrix(1, NSPAR, 1, 1);
+        for (nt = 1; nt <= NSPAR; nt++) {
+            srcpos_current[nt][1] = acq->srcpos[nt][ishot];
+        }
         /* find this single source positions on subdomains  */
-        if (acq->nsrc_loc > 0)
-            free_matrix(acq->srcpos_loc, 1, NSPAR, 1, 1);
         acq->srcpos_loc = splitsrc(srcpos_current, &acq->nsrc_loc, 1, gv);
+        free_matrix(srcpos_current, 1, NSPAR, 1, 1);
     } else {
-        /* Distribute source positions on subdomains */
+        /* distribute source positions on subdomains */
         acq->srcpos_loc = splitsrc(acq->srcpos, &acq->nsrc_loc, acq->nsrc, gv);
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
 
-    /* calculate wavelet for each source point. Only done, if there is
-     * no source in the current domain */
+    /* calculate wavelet for source point(s) */
     if (acq->nsrc_loc > 0) {
         wavelet(acq, gv);
     }
 
     /* write source wavelet to file in subdomain that contains source */
-    if (acq->signals && 1 == gv->SIGOUT) {
+    if (acq->nsrc_loc > 0 && 1 == gv->SIGOUT) {
         switch (gv->SIGOUT_FORMAT) {
           case 1:
               sprintf(file_ext, "su");
@@ -75,14 +78,18 @@ void initsrc(int ishot, int nshots, AcqVar *acq, GlobVar *gv)
               log_fatal("Unknown SIGOUT_FORMAT encountered.\n");
               break;
         }
-        dummy = imatrix(1, 3, 1, 1);
-        dummy[1][1] = iround(acq->srcpos_loc[1][ishot] / gv->DH);
-        dummy[2][1] = iround(acq->srcpos_loc[2][ishot] / gv->DH);
+        int **dummy = imatrix(1, 3, 1, 1);
+        dummy[1][1] = (int)(acq->srcpos_loc[1][1]) - 1 + gv->GGRID[1];
+	dummy[2][1] = (int)(acq->srcpos_loc[2][1]) - 1 + gv->GGRID[3];
         dummy[3][1] = 0;
         sprintf(sigf, "%s.shot%d.%s", gv->SIGOUT_FILE, ishot, file_ext);
         log_info("Writing source wavelet to file %s.\n", sigf);
-        outseis_glob(fopen(sigf, "w"), acq->signals, dummy, 1, acq->srcpos_loc, gv->NT, gv->SIGOUT_FORMAT, ishot, 0,
-                     gv);
+        FILE *fid = fopen(sigf, "w");
+        if (!fid) {
+            log_warn("Could not open %s for writing - no signature written.\n", sigf);
+        } else {
+            outseis_glob(fid, acq->signals, dummy, 1, acq->srcpos, gv->NT, gv->SIGOUT_FORMAT, ishot, 0, gv);
+        }
         free_imatrix(dummy, 1, 3, 1, 1);
     }
 }
