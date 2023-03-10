f2273caa76691f4586f18cf292c0c92000720f5a
Thomas Hertweck <Thomas.Hertweck@kit.edu>
2023-03-10 12:00:43 +0100

    Updated harmonic averaging to handle situation where negative values are encountered and infinity could occur.

--- src/av_mue.c
+++ src/av_mue.c
@@ -24,15 +24,19 @@
 
 #include "fd.h"
 
-void av_mue(float **u, float **uipjp, GlobVar * gv)
+void av_mue(float **u, float **uipjp, GlobVar *gv)
 {
+    double sum;
     for (int j = 1; j <= gv->NY; j++) {
         for (int i = 1; i <= gv->NX; i++) {
             if ((u[j][i] == 0.f) || (u[j][i + 1] == 0.f) || (u[j + 1][i] == 0.f) || (u[j + 1][i + 1] == 0.f)) {
                 uipjp[j][i] = 0.f;
             } else {
-                uipjp[j][i] =
-                    4.0 / ((1.0 / u[j][i]) + (1.0 / u[j][i + 1]) + (1.0 / u[j + 1][i]) + (1.0 / u[j + 1][i + 1]));
+                sum = (1.0 / u[j][i]) + (1.0 / u[j][i + 1]) + (1.0 / u[j + 1][i]) + (1.0 / u[j + 1][i + 1]);
+                if (sum != 0.0)
+                    uipjp[j][i] = 4.0 / sum;
+                else
+                    uipjp[j][i] = 0.0;
             }
         }
     }
